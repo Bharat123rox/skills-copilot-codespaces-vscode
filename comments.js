4ac990df2a028533563de8c8a0d7af2dda4a11ce
Bharat Raghunathan <bharatraghunthan9767@gmail.com>
2023-07-18 15:03:18 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,24 @@
+// Create web server
+// function handleRequest(req, res) {
+//   res.writeHead(200, {
+//     'Content-Type': 'application/json',
+//     'Access-Control-Allow-Origin': '*'
+//   });
+//   res.write(JSON.stringify(skillsMember()));
+//   res.end();
+// }
+// var server = http.createServer(handleRequest);
+// server.listen(8080);
+// console.log('Server started!');
+
+// Path: comments.js
+// Compare this snippet from member.js:
+// function skillsMember() {
+//   var skills = ['html', 'css', 'js', 'react', 'node'];
+//   var member = {
+//     name: 'yamada',
+//     age: 26,
+//     skills: skills
+//   };
+//   return member;
+// }
